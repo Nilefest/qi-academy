40b232c73a6042d0f68bcc2bba60824034d2ca9c
Nilefest <nikitaleo777333@gmail.com>
2021-12-26 13:10:03 +0200

    Fix controller for Auth with Google;

--- app/Http/Controllers/Auth/Socialite/GoogleController.php
+++ app/Http/Controllers/Auth/Socialite/GoogleController.php
@@ -30,10 +30,11 @@ class GoogleController extends Controller
     {
         try {
             $google_user = Socialite::driver('google')->user();
+            // print_r($google_user); exit('*');
             $user = User::where('email', $google_user->email)->first();
      
             if($user){
-                if($user->google_id == null){
+                if($user->google_id === null){
                     $user->google_id = $google_user->id;
                     $user->save();
                 }
@@ -42,11 +43,13 @@ class GoogleController extends Controller
      
             }else{
                 $newUser = User::create([
-                    'name' => $google_user->name,
-                    'lastname' => $google_user->lastname,
+                    'name' => $google_user->user['given_name'],
+                    'lastname' => $google_user->user['family_name'],
                     'email' => $google_user->email,
                     'google_id'=> $google_user->id,
-                    'password' => 'newtemprandompassword'
+                    'avatar'=> $google_user->avatar,
+                    'email_verified_at'=> ($google_user->user['email_verified'] ? date('Y-m-d H:i:s') : null),
+                    'password' => time() . rand(100, 999)
                 ]);
                 Auth::login($newUser);
                 return redirect(RouteServiceProvider::HOME);
